f46dc8a5a4f8c63ba7d5692c1e78592fe3e57b40
kendo <kim@MB-Pro-KJ-2.local>
2015-05-21 12:11:02 +0900

    change config

--- config.php
+++ config.php
@@ -4,12 +4,12 @@ if ( !defined('PATH_HTTPROOT') ) {
     define( 'PATH_HTTPROOT', $_SERVER['DOCUMENT_ROOT'] );
 }
 if ( !defined('PATH_OUTROOT') ) {
-    // define( 'PATH_OUTROOT', PATH_HTTPROOT."/imgout" );  
-    define( 'PATH_OUTROOT', "imgout" );          
+    define( 'PATH_OUTROOT', PATH_HTTPROOT."/imgout" );  
+    // define( 'PATH_OUTROOT', "imgout" );          
 }
 if ( !defined('PATH_INROOT') ) {
-    // define( 'PATH_INROOT', PATH_HTTPROOT."/imgin" );     
-    define( 'PATH_INROOT', "imgin" );
+    define( 'PATH_INROOT', PATH_HTTPROOT."/imgin" );     
+    // define( 'PATH_INROOT', "imgin" );
 }
 
 date_default_timezone_set('Asia/Tokyo');
