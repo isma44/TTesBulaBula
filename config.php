500852efa41ba1139ff26ee1c2b28b44afbd8977
palcon <isma44@gmail.com>
2015-05-27 10:20:11 +0900

    custom2 branch commit

--- config.php
+++ config.php
@@ -1,18 +1,21 @@
 <?php   
 
+
 if ( !defined('PATH_HTTPROOT') ) {
     define( 'PATH_HTTPROOT', $_SERVER['DOCUMENT_ROOT'] );
 }
+
 if ( !defined('PATH_OUTROOT') ) {
     // define( 'PATH_OUTROOT', PATH_HTTPROOT."/imgout" );  
     define( 'PATH_OUTROOT', "imgout" );          
 }
+
 if ( !defined('PATH_INROOT') ) {
     // define( 'PATH_INROOT', PATH_HTTPROOT."/imgin" );     
     define( 'PATH_INROOT', "imgin" );
 }
 
 date_default_timezone_set('Asia/Tokyo');
-
+// from custom2 branch
 
 ?>
